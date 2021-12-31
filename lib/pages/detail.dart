269b23ac94ba60bbbe2ee6e04adafe8c7b7d1ef9
Ying Zheng <ying.zheng@Yings-MacBook-Pro.local>
2021-12-31 14:45:48 +0800

    提交图片组件

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -7,7 +7,43 @@ class Detail extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("detail page")),
-        body: const Text('这里是牙齿图片详情页!'),
+        body: MainContent(),
       );
+  }
+}
+
+class MainContent extends StatefulWidget {
+  const MainContent({Key? key}) : super(key: key);
+
+  @override
+  _MainContentState createState() => _MainContentState();
+}
+
+class _MainContentState extends State<MainContent> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Container(
+            width: 500,
+            height: 500,
+            child: Image.asset("assets/pic/img001.png"),
+          ),
+          ElevatedButton(
+            child: Text("放大"),
+            onPressed: () {
+              print('点击放大');
+            },
+          ),
+          ElevatedButton(
+            child: Text("右旋90度"),
+            onPressed: () {
+              print('旋转');
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
