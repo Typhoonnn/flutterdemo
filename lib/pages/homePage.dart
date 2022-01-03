130f6ef0f1be86a7257f621cc96ed8c4e52d218a
yangchen <13488926667@163.com>
2022-01-03 23:53:49 +0800

    goodsList

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -22,6 +22,9 @@ class list extends StatelessWidget {
           child: ListTile(
             leading: Image.network('http://5b0988e595225.cdn.sohucs.com/images/20180816/9377165a1b39438dab1ff74e581000a5.jpeg'),
             title: Text('牙釉质能够生长出一些细小的缝隙，他们能够分散咀嚼的压力，同时保护牙齿断裂。航天工程师正在研究这种机制，以便能够发明更好的宇宙飞船。'),
+            onTap: () {
+              Navigator.pushNamed(context, "/imgList");
+            }
           ),
         ),
         Container(
@@ -29,6 +32,9 @@ class list extends StatelessWidget {
           child: ListTile(
             leading: Image.network('http://5b0988e595225.cdn.sohucs.com/images/20180816/9377165a1b39438dab1ff74e581000a5.jpeg'),
             title: Text('氟能够保护牙齿衰老。因为它能够阻止牙釉质中矿物质的流失，促进牙釉质吸收唾液中的钙和磷酸盐。'),
+            onTap: () {
+              Navigator.pushNamed(context, "/imgList");
+            }
           ),
         ),
         Container(
@@ -36,6 +42,9 @@ class list extends StatelessWidget {
           child: ListTile(
             leading: Image.network('http://5b0988e595225.cdn.sohucs.com/images/20180816/9377165a1b39438dab1ff74e581000a5.jpeg'),
             title: Text('1945年，密歇根的大溪城是世界上**个在供水系统中加氟的城市。现在，美国三分之二的居民都能用到加氟的水。'),
+            onTap: () {
+              Navigator.pushNamed(context, "/imgList");
+            }
           ),
         ),
         Container(
@@ -43,6 +52,9 @@ class list extends StatelessWidget {
           child: ListTile(
             leading: Image.network('http://5b0988e595225.cdn.sohucs.com/images/20180816/9377165a1b39438dab1ff74e581000a5.jpeg'),
             title: Text('保护牙齿的战斗进行到哪一步了？佛罗里达大学的口腔学退休教授杰弗瑞·哈里曼已经着手进行变异链球菌的人体试验了。它是一种口腔细菌的基因变种，会产生比较少的酸。'),
+            onTap: () {
+              Navigator.pushNamed(context, "/imgList");
+            }
           ),
         ),
         Container(
@@ -50,6 +62,9 @@ class list extends StatelessWidget {
           child: ListTile(
             leading: Image.network('http://5b0988e595225.cdn.sohucs.com/images/20180816/9377165a1b39438dab1ff74e581000a5.jpeg'),
             title: Text('但这其实是牙医的鬼伎俩：这种植牙通常都失败，而且会使牙齿更糟糕，因为移植的牙齿多半带有梅毒。'),
+            onTap: () {
+              Navigator.pushNamed(context, "/imgList");
+            }
           ),
         ),
         Container(
@@ -57,6 +72,9 @@ class list extends StatelessWidget {
           child: ListTile(
             leading: Image.network('http://5b0988e595225.cdn.sohucs.com/images/20180816/9377165a1b39438dab1ff74e581000a5.jpeg'),
             title: Text('我们的牙齿并不能抵御我们嘴巴里的细菌。这些细菌会产生酸，他们会侵蚀牙釉质里的钙和磷酸盐，这就会导致龋齿。'),
+            onTap: () {
+              Navigator.pushNamed(context, "/imgList");
+            }
           ),
         )
       ]
