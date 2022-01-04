45a284d58ee36329e94a7d82813c4cc7324c6091
xisen.he <xisen.he@thoughtworks.com>
2022-01-04 10:14:35 +0800

    fix: imgList page bug

--- lib/pages/imgList.dart
+++ lib/pages/imgList.dart
@@ -62,21 +62,22 @@ class Imgs extends State {
 }
 
 
-final List<String> images = [
-    "https://img.iplaysoft.com/wp-content/uploads/2019/free-images/free_stock_photo.jpg",
-    "https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg",
-    "https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=20&m=825383494&s=612x612&w=0&h=tEqZ5HFZcM3lmDm_cmI7hOeceiqy9gYrkyLTTkrXdY4=",
-    "https://media.istockphoto.com/photos/sunset-with-pebbles-on-beach-in-nice-france-picture-id1157205177?k=20&m=1157205177&s=612x612&w=0&h=bmCFFtaLRtF_eYhjZ3FkhPXU3X-yrdvr85xvl2CmQ9g=",
-    "https://media.macphun.com/img/uploads/customer/how-to/579/15531840725c93b5489d84e9.43781620.jpg?q=85&w=1340",
-    "https://static.independent.co.uk/2021/04/05/12/SWNS_SOCIETY_PHOTOS_03.jpg?width=982&height=726&auto=webp&quality=75",
-    "https://media.istockphoto.com/photos/lily-lake-fiery-sunset-picture-id1299550772?b=1&k=20&m=1299550772&s=170667a&w=0&h=9Zl6mBq0l8wUDmIghk4hkhmyYcQQsJyqM1HJjRULQSI=",
-    "https://www.groovypost.com/wp-content/uploads/2018/06/taking-photo-picture-landmark-phone-search-featured.jpg",
-];
-final rng = Random();
-
 class Img extends StatelessWidget {
 
     String url = '';
+
+    final List<String> images = [
+        "https://img.iplaysoft.com/wp-content/uploads/2019/free-images/free_stock_photo.jpg",
+        "https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg",
+        "https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=20&m=825383494&s=612x612&w=0&h=tEqZ5HFZcM3lmDm_cmI7hOeceiqy9gYrkyLTTkrXdY4=",
+        "https://media.istockphoto.com/photos/sunset-with-pebbles-on-beach-in-nice-france-picture-id1157205177?k=20&m=1157205177&s=612x612&w=0&h=bmCFFtaLRtF_eYhjZ3FkhPXU3X-yrdvr85xvl2CmQ9g=",
+        "https://media.macphun.com/img/uploads/customer/how-to/579/15531840725c93b5489d84e9.43781620.jpg?q=85&w=1340",
+        "https://static.independent.co.uk/2021/04/05/12/SWNS_SOCIETY_PHOTOS_03.jpg?width=982&height=726&auto=webp&quality=75",
+        "https://media.istockphoto.com/photos/lily-lake-fiery-sunset-picture-id1299550772?b=1&k=20&m=1299550772&s=170667a&w=0&h=9Zl6mBq0l8wUDmIghk4hkhmyYcQQsJyqM1HJjRULQSI=",
+        "https://www.groovypost.com/wp-content/uploads/2018/06/taking-photo-picture-landmark-phone-search-featured.jpg",
+    ];
+
+    final rng = Random();
 
     Img(){
         int index = rng.nextInt(images.length);
