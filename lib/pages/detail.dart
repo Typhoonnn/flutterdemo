587a40d19ec0fffaea754990c6f37c53d13fd4a2
Ying Zheng <ying.zheng@Yings-MacBook-Pro.local>
2022-01-05 10:10:56 +0800

    删除多余代码

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -1,11 +1,13 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
-// void main() => runApp(MyApp());
+
 class Detail extends StatefulWidget {
+  const Detail({Key? key}) : super(key: key);
+
   @override
   _DetailState createState() => _DetailState();
 }
-class _DetailState extends State<Detail> with TickerProviderStateMixin {
+class _DetailState extends State<Detail> {
   double rotateVal = 0.0;
 
   @override
@@ -19,31 +21,27 @@ class _DetailState extends State<Detail> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-            home: Container(
-              child: Column(
-                children: [
-                  const SizedBox(height: 100,),
-                  Container(
-                    width: 300,
-                    height: 300,
-                    child: GestureDetector(
-                      onTap: _addAngle,
-                      child: TweenAnimationBuilder(
-                        duration: const Duration(milliseconds: 500),
-                        tween: Tween(end: rotateVal), 
-                        builder: (BuildContext context, double value, Widget? child) {
-                          return Container(
-                            child: Transform.rotate(
-                                angle: value,
-                                child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnRQlEsOnBqoLuKHuNYUSv_l4O0GnnRlSE7DZHBwDLPUZWDdE9ufxABuDs_lm35hCwFk0&usqp=CAU", fit: BoxFit.fill,),
-                              ),
+            home: Column(
+              children: [
+                const SizedBox(height: 100,),
+                SizedBox(
+                  width: 500,
+                  height: 500,
+                  child: GestureDetector(
+                    onTap: _addAngle,
+                    child: TweenAnimationBuilder(
+                      duration: const Duration(milliseconds: 500),
+                      tween: Tween(end: rotateVal), 
+                      builder: (BuildContext context, double value, Widget? child) {
+                        return Transform.rotate(
+                            angle: value,
+                            child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnRQlEsOnBqoLuKHuNYUSv_l4O0GnnRlSE7DZHBwDLPUZWDdE9ufxABuDs_lm35hCwFk0&usqp=CAU", fit: BoxFit.fill,),
                           );
-                        }, 
-                      ),
+                      }, 
                     ),
-                  )
-                ],
-              )
+                  ),
+                )
+              ],
             ),
           );
   }
